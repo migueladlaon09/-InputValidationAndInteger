7c2c025a93d6705cd17566b10c176efae4cf9385
migueladlaon09 <168712051+migueladlaon09@users.noreply.github.com>
2025-08-11 10:15:05 +0800

    Act5IntComputation.dart
    Activty 5

---
+++ Act5IntComputation.dart
@@ -0,0 +1,18 @@
+void main() {
+  print("===Basic Calculator===");
+
+  print("Enter First Number: ");
+  int? fnumber = int.parse(stdin.readLineSync()!);
+  print("Enter Second Number: ");
+  int? snumber = int.parse(stdin.readLineSync()!);
+
+  int addition =fnumber+snumber;
+  int subtraction = fnumber-snumber;
+  int multiplication =fnumber*snumber;
+  double division =fnumber/snumber;
+
+  print("The Sum of: ${fnumber}+${snumber}= ${addition}");
+  print("The difference of: ${fnumber}-${snumber}= ${subtraction}");
+  print("The Product of: ${fnumber}*${snumber}= ${multiplication}");
+  print("The Quotient of: ${fnumber}/${snumber}= ${division}");
+}
