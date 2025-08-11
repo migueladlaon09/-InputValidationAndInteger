cd2b2eb2ba80f61f784dbdbe695edec7439af2ef
migueladlaon09 <168712051+migueladlaon09@users.noreply.github.com>
2025-08-11 10:13:58 +0800

    Create Act4UserInput.dart

---
+++ Act4UserInput.dart
@@ -0,0 +1,38 @@
+void main() {
+ print("Enter Number: ");
+
+ int? number = int.parse(stdin.readLineSync()!);
+
+ print("The Entered Number Is: ${number}");
+}
+
+void main() {
+  print("==Identifier==");
+  
+  print("Enter your Age: ");
+  String? input = stdin.readLineSync();
+  int? number;
+
+  if (input != null && input.trim().isNotEmpty) {
+    if (int.tryParse(input.trim()) != null) {
+      number = int.parse(input.trim());
+    } 
+  }
+
+
+  print("\n== Validation ==");
+  print("Age: ${number}");
+
+  
+  if (number != null) {
+    if (number >= 60) {
+      print("You are an Senior");
+      }else if (number >= 18) {
+      print("You are an Adult");
+      } else {
+      print("You are a Minor");
+      }
+  } else {
+    print("Please Provide a Valid number");
+  }
+}
